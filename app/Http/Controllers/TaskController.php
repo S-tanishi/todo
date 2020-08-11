6579d06cc7d06d568e39e4b7a4d2f4bd8f39b60f
S-tanishi <66398847+S-tanishi@users.noreply.github.com>
2020-08-11 16:00:50 +0900

    controller

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -8,6 +8,10 @@ class TaskController extends Controller
 {
 　   public function index()
      {
-        return "Hello world"
+        $folders = Folder::all();
+
+        return view('tasks/index', [
+            'folders' => $folders,
+        ]);
      }
 }
