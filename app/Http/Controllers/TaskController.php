ff85cbb15e63b90037201c095c47447305d86c4c
S-tanishi <66398847+S-tanishi@users.noreply.github.com>
2020-08-16 15:27:24 +0900

    テンプレートの部品化

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -50,4 +50,16 @@ class TaskController extends Controller
         ]);
     
     }
+
+    /**
+     *  GET /folders/{id}/tasks/{task_id}/edit
+     */
+    public function showEditForm(int $id, int $task_id)
+    {
+        $task =Task::find($task_id);
+
+        return view('tasks/edit', [
+            'task' => $task,
+        ]);
+    }
 }
