9d7eeaaaa53992f7405b28047cec25ecab9cbe26
S-tanishi <66398847+S-tanishi@users.noreply.github.com>
2020-08-26 10:19:53 +0900

    フォルダ作成処理をブラッシュアップ

--- app/Http/Controllers/FolderController.php
+++ app/Http/Controllers/FolderController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Folder;
 use App\Http\Requests\CreateFolder;
 use Illuminate\Http\Request;
+// Authクラスをインポートする
+use Illuminate\Support\Facades\Auth;
 
 class FolderController extends Controller
 {
@@ -19,6 +21,10 @@ class FolderController extends Controller
         $folder = new Folder();
         // タイトルに入力値を代入する
         $folder->title = $request->title;
+
+        // ユーザーに紐づけて保存
+        Auth::user()->folders()->save($folder);
+        
         // インスタンスの状態をデータベースに書き込む
         $folder->save();
 
@@ -26,4 +32,6 @@ class FolderController extends Controller
             'id' => $folder->id,
         ]);
     }
+
+
 }
