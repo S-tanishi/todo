6f63b0adb0387ab800160293766c955a601ca504
S-tanishi <66398847+S-tanishi@users.noreply.github.com>
2020-08-26 10:31:06 +0900

    ホームページをブラッシュアップ

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,6 +9,20 @@ class HomeController extends Controller
 {
     public function index()
     {
-        return view('home');
+        // ログインユーザーを取得
+        $user = Auth::user();
+
+        // ログインユーザーに紐づくフォルダを一つ取得
+        $folder = $user->folder()->first();
+
+        // まだ１つもフォルダを作成していなければホームページをリスポンスする
+        if (is_null($folder)) {
+            return view('home');
+        }
+
+        //フォルダがある場合そのフォルダのタスク一覧にリダイレクトする
+        return redirect()->route('tasks.index', [
+            'id' => $folder->id.
+        ]);
     }
 }
